9a2eada0c01ca9c5b59c4dd701d31c6c6d94523c
gabris.attila <007gabrisati@gmail.com>
2023-08-24 15:17:08 +0200

    Third puzzle second part

--- 2022/src/ThirdPuzzle.java
+++ 2022/src/ThirdPuzzle.java
@@ -12,8 +12,7 @@ public class ThirdPuzzle {
     }
 
     private void part(boolean isFirst) throws IOException {
-        if (isFirst)
-            System.out.println(sumOfCommonsInStrings(new File("2022/src/Third.txt")));
+        System.out.println(sumOfCommonsInStrings(new File("2022/src/Third.txt"), isFirst));
     }
 
     private int charToInt(char ch){
@@ -23,17 +22,26 @@ public class ThirdPuzzle {
             return (int)ch - (65 - 27);
     }
 
-
-
-
     //First
 
-    private int sumOfCommonsInStrings(File file) throws IOException {
+    private int sumOfCommonsInStrings(File file, boolean isFirst) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line = "";
+        String line;
+        ArrayList<String> lines = new ArrayList<>();
+        int i = 0;
         int sum = 0;
         while ((line = reader.readLine()) != null){
-            sum += findCommonCharInInt(line.substring(0, (line.length() / 2)), line.substring((line.length() / 2)));
+
+            if (isFirst)
+                sum += findCommonCharInInt(line.substring(0, (line.length() / 2)), line.substring((line.length() / 2)));
+            else {
+                lines.add(line);
+                if (lines.size() == 3){
+                    sum += findCommonCharInInt(lines.get(0), lines.get(1), lines.get(2));
+                    lines.clear();
+                }
+            }
+
         }
         return sum;
     }
@@ -48,5 +56,19 @@ public class ThirdPuzzle {
         return i1.get(0);
     }
 
-
+    private int findCommonCharInInt(String s1, String s2, String s3){
+        ArrayList<Integer> i1 = new ArrayList<>(), i2 = new ArrayList<>(), i3 = new ArrayList<>();
+        for (int i = 0; i < s1.length(); i++) {
+            i1.add(charToInt(s1.charAt(i)));
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            i2.add(charToInt(s2.charAt(i)));
+        }
+        for (int i = 0; i < s3.length(); i++) {
+            i3.add(charToInt(s3.charAt(i)));
+        }
+        i1.retainAll(i2);
+        i1.retainAll(i3);
+        return i1.get(0);
+    }
 }
