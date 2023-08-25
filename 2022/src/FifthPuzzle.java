f63d87dea03d6f892459bfaf3d2ba20b77bbd9f6
gabris.attila <007gabrisati@gmail.com>
2023-08-25 15:50:11 +0200

    Fifth puzzle part

--- 2022/src/FifthPuzzle.java
+++ 2022/src/FifthPuzzle.java
@@ -15,19 +15,26 @@ public class FifthPuzzle {
     public FifthPuzzle() throws IOException {
         crateStacks = new HashMap<>();
         fillMap(new File("2022/src/Fifth"));
-        part(true, new File("2022/src/Fifth"));
+//        part(true, new File("2022/src/Fifth"));
+        part(false, new File("2022/src/Fifth"));
 
     }
 
     void part(boolean isFirst, File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
         if (isFirst){
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
             while((line = reader.readLine()) != null){
-                move(line);
+                move9000(line);
             }
             for (int i = 0; i < crateStacks.size(); i++) {
-                reverse(crateStacks.get(i + 1));
+                System.out.print(crateStacks.get(i + 1).get(crateStacks.get(i + 1).size() - 1));
+            }
+        }else {
+            while((line = reader.readLine()) != null){
+                move9001(line);
+            }
+            for (int i = 0; i < crateStacks.size(); i++) {
                 System.out.print(crateStacks.get(i + 1).get(crateStacks.get(i + 1).size() - 1));
             }
         }
@@ -37,7 +44,7 @@ public class FifthPuzzle {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line;
         int i = 0;
-        int length = 0;
+        int length;
         while((line = reader.readLine()) != null){
             if (i == 0){
                 length = (line.length() - 3) / 4 + 1;
@@ -54,15 +61,32 @@ public class FifthPuzzle {
             }
             i++;
         }
+        for (int j = 1; j < crateStacks.size() + 1; j++) {
+            reverse(crateStacks.get(j));
+        }
+        for (int j = 1; j < crateStacks.size() + 1; j++) {
+            System.out.println(crateStacks.get(j));
+        }
     }
 
-    private void move(String moveLine){
+    private void move9000(String moveLine){
         if (!moveLine.isEmpty() && moveLine.charAt(0) == 'm'){
             String[] parts = moveLine.split(" ");
             int times = Integer.parseInt(parts[1]), from = Integer.parseInt(parts[3]), to = Integer.parseInt(parts[5]);
             for (int i = 0; i < times; i++) {
                 crateStacks.get(to).add(crateStacks.get(from).get(crateStacks.get(from).size() - 1));
                 crateStacks.get(from).remove(crateStacks.get(from).size() - 1);
+            }
+        }
+    }
+
+    private void move9001(String moveLine) {
+        if (!moveLine.isEmpty() && moveLine.charAt(0) == 'm'){
+            String[] parts = moveLine.split(" ");
+            int howMany = Integer.parseInt(parts[1]), from = Integer.parseInt(parts[3]), to = Integer.parseInt(parts[5]);
+            for (int i = howMany; i > 0; i--) {
+                crateStacks.get(to).add(crateStacks.get(from).get(crateStacks.get(from).size() - i));
+                crateStacks.get(from).remove(crateStacks.get(from).size() - i);
             }
         }
     }
